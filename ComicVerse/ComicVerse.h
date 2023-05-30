75da59945bb3a0466c4630da7286f50684d26b9a
Raphael Torquato <raphael.martini@live.com>
2023-05-30 13:49:48 -0300

    default project
    default project

---
+++ ComicVerse/ComicVerse.h
@@ -0,0 +1,18 @@
+//
+//  ComicVerse.h
+//  ComicVerse
+//
+//  Created by Raphael Torquato on 30/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComicVerse.
+FOUNDATION_EXPORT double ComicVerseVersionNumber;
+
+//! Project version string for ComicVerse.
+FOUNDATION_EXPORT const unsigned char ComicVerseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComicVerse/PublicHeader.h>
+
+
